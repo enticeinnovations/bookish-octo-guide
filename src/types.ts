2ff5690acbb92473feecabe62dfe3218d004091a
Adarshvasuu <vasuadarsh52@gmail.com>
2026-08-20 11:43:23 +0530

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-export type PageType = 'home' | 'about' | 'services' | 'blogs' | 'contact';
+export type PageType = 'home' | 'about' | 'services' | 'blogs' | 'contact' | 'terms';
 
 export interface ServiceItem {
   id: string;
